f346212137b266a3438f54bfd4277eb3f1e2e0c0
Saurav Pathak <sauravpathak31@gmail.com>
2023-05-09 20:28:15 -0500

    feat: added eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -13,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
