bf53bf3df728407b118e2561cebb28e76aaa451a
Saurav Pathak <sauravpathak31@gmail.com>
2023-06-07 22:00:11 -0500

    feat: added profile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Logo from '../logo/Logo';
 import './navbar.css';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Profile from '../profile/Profile';
 
 const Navbar = () => {
   return (
     <div className='navbar' style={{'backgroundColor':'green'}}>
       <Logo className="ms-2"/>
+      <Profile/>
       <ShoppingCartOutlinedIcon fontSize='large' className='me-2'/>
     </div>
   );
