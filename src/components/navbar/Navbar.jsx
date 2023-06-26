23499b47633a2d2320419cf2f063fc8cea3cf587
Saurav Pathak <37608291+SauravPathak1@users.noreply.github.com>
2023-06-25 22:13:38 -0500

    feat: added profile (#23)
    * feat: added profile
    
    * feat: added profile
    
    * feat:added person icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,12 +2,15 @@
 import React from 'react';
 import Logo from '../logo/Logo';
 import './navbar.css';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Profile from '../profile/Profile';
 
 const Navbar = () => {
   return (
-    <div className='navbar ms-2'>
-      <Logo/>
-
+    <div className='navbar' style={{'backgroundColor':'green'}}>
+      <Logo className="ms-2"/>
+      <Profile/>
+      <ShoppingCartOutlinedIcon fontSize='large' className='me-2'/>
     </div>
   );
 };
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,9 @@
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+
+const Profile = () => {
+  return (
+    <div><PersonOutlineOutlinedIcon/></div>
+  );
+};
+
+export default Profile;
