edf7ed4848f8b55db448f83e16e6f1de67ee1d12
Saurav Pathak <sauravpathak31@gmail.com>
2023-06-25 22:12:07 -0500

    feat:added person icon

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div><PersonOutlineOutlinedIcon/></div>
   );
 };
 
