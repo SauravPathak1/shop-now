032e7fe4ae54bebc1fcf6545bc940da97885bce1
Saurav Pathak <37608291+SauravPathak1@users.noreply.github.com>
2023-07-01 20:41:22 -0500

    feat: added routes (#25)

--- src/components/userprofile/UserProfile.jsx
+++ src/components/userprofile/UserProfile.jsx
@@ -1,23 +1,20 @@
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import './userprofile.css';
+import { Divider } from '@mui/material';
 
 const UserProfile = () => {
-  const [profileDropdownOpen, setProfileDropdownOpen] = useState(true);
-  const handleProfileButtonClick= () =>{
-    setProfileDropdownOpen(false);
-  };
   return (
-    <div className='ms-2 me-2'>
-      <button onClick={handleProfileButtonClick}>
+    <div className='profile-dropdown'>
+      <button className="dropbtn">
         <PersonOutlineOutlinedIcon fontSize='large'/>
-        {
-          profileDropdownOpen && (
-            <div>
-                hererer
-            </div>
-          )}
       </button>
+      <div className="dropdown-content">
+        <a href="/login">Login / Sign In</a>
+        <Divider sx={{ my: 0.5, bgcolor: 'black' }} />
+        <a href="/orders">Your Orders</a>
+        <a href="/profile">Profile</a>
+      </div>
     </div>
   );
 };
